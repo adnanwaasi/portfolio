2f686bc300f7929e9002c0cef57208eef5c663e7
Adnan Waasi <114774841+adnanwaasi@users.noreply.github.com>
2025-08-10 14:00:42 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: 'github.com/adnanwaasi/portfolio', // The name of your GitHub repository
+  base: 'https://github.com/adnanwaasi/portfolio/', // The name of your GitHub repository
 });
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// export default defineConfig({
+//   plugins: [react()],
+// })
